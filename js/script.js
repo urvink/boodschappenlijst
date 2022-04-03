cc0f6df28d2dd3e8dea0c3d8e90f3bf67324f8b5
UrvinK <urvinkirindongo@gmail.com>
2022-04-03 18:08:56 +0200

    Init

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).on('click','#dec_qty',()=>{
+    let qty = $('#qty').val();
+    if (qty>=2) {
+        qty--;
+        $('#qty').val(qty)
+    }    
+});
+
+$(document).on('click', '#inc_qty', () => {
+    let qty = $('#qty').val();
+    if (qty <= 98) {
+        qty++;
+        $('#qty').val(qty)
+    }
+    
+});
+
+$('#totalAmount')
