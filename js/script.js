2d2cc946dcbc85654627c5c6c5a9f385a3c38e11
UrvinK <urvinkirindongo@gmail.com>
2022-04-06 02:09:54 +0200

    Building out items, adding logic

--- js/script.js
+++ js/script.js
@@ -16,5 +16,30 @@ $(document).on('click', '#inc_qty', ()=>{
 });
 
 $(document).on('click', '#addItem', ()=>{
-    alert("HEY u");
+    //Vars
+    let store = $('#store :selected').val();
+    let product = $('#addProduct').val();
+    let qty = $('#qty').val();
+    let price = $('#price').val();
+    
+   if (store.length!=0 || product.length!=0 || price.length!=0) {
+       
+   }
+
+    alert(`Store: ${store}, Product: ${product}, Qty: ${qty}, Price: ${price}.....Total: ${qty*price}`);
+
 });
+
+function calcTotal(newSubTotal) {
+    let total = $('#totalAmount').val();
+
+    total += newSubTotal;
+
+    $('#totalAmount').val(total);    
+}
+
+function checkInputs() {
+    if (condition) {
+        name
+    }
+}
